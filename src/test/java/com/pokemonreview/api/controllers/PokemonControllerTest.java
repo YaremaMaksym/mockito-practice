124af7fb963f8f90c34a9ee93e12ed31ca4890af
Maksym Yarema <macsym2@gmail.com>
2023-08-30 12:07:28 +0300

    feat: setup PokemonControllerTest.class

---
+++ src/test/java/com/pokemonreview/api/controllers/PokemonControllerTest.java
@@ -0,0 +1,26 @@
+package com.pokemonreview.api.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pokemonreview.api.service.PokemonService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = PokemonController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class PokemonControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PokemonService pokemonService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+}
